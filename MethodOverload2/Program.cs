1316a0aaa8bf18e1c5fce6b5e1c50dac756a15a3
Stephen Madalo <stephenmadalo89@gmail.com>
2022-09-24 11:53:47 -0400

    initial commit

---
+++ MethodOverload2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+
+        
+
+
+
+
+
+
+
+
+            static int Add(int numA, int numB)
+            {
+                return numA + numB;
+            }
+            static decimal Add(decimal numA, decimal numB)
+            {
+                return (numA + numB);
+            }
+            static string Add(int numA, int numB, bool isTrue)
+            {
+                var sum = 0;
+                var answer = "";
+
+                if (isTrue)
+                {
+                    sum = numA + numB;
+
+                    if (sum == 1)
+                    {
+                        answer = $"{sum} dollar";
+                    }
+                    else
+                    {
+                        answer = $"{sum} dollars";
+                    }
+
+
+
+                }
+
+                return answer;
+        
+        }
+
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine(Add(-5, 6, true));
+
+
+            Console.WriteLine(Add(-9, 1, true));
+
+
+            Console.WriteLine(Add(9, 1, true));
+
+
+
+
+
+
+        }
+    }
+}
